f20a76cfec4fec6a8466ff6df6b53d2d6f044b4a
BrentBauman86 <brentbauman86@gmail.com>
2018-10-23 10:25:51 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,23 @@
 class UsersController < ApplicationController
 
     def new
-
-
-
     end
 
-    def create 
-    
+    def create  
+        @user = User.create(user_params)
+        return redirect_to controller: 'users', action: 'new' unless @user.save
+        session[:user_id] = @user.id 
         flash[:notice] = "User Successfully Created"
-        redirect_to @post
+
+        redirect_to controller: 'users', action: 'home'
     end
 
     def home
-        "Hi, #{@user.name}."
+    end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:name, :password, :password_confirmation)
     end
 end
